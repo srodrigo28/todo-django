88f17ff4e6bb64184be99290d222dec18eb8c8f5
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2026-01-01 11:26:03 -0300

    criado a rota listar.html

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Tarefa
 
 # Create your views here.
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all()
+    return render(request, 'tarefas/listar.html', {'tarefas': tarefas})
